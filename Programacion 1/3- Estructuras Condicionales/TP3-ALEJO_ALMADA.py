aa2d22e6c28d8f1599c3ce762d2f2029c9ef8a60
Alejo Almada <alejonahuelalmada@gmail.com>
2025-08-28 20:02:47 -0300

    Se organizo carpeta para ver mas limpia

---
+++ Programacion 1/3- Estructuras Condicionales/TP3-ALEJO_ALMADA.py
@@ -0,0 +1,7 @@
+# 1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+# deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”
+
+# MAYOR_EDAD = 18
+# edad = int(input("Ingrese su edad: "))
+# if edad >= MAYOR_EDAD:
+#     print("Es mayor de edad.") 
--- README.md
+++
@@ -1,3 +0,0 @@
-# UTN-TUPaDProgramacion1
-Trabajo Practico 1 realizado con python
-Se subiran los ejercicios con Python
