06d8904bb40cf2ab66ca832df7187aea432e7499
Alejo Almada <alejonahuelalmada@gmail.com>
2025-08-28 20:25:17 -0300

    Se realizo hasta el ejercicio 4

--- Programacion 1/3- Estructuras Condicionales/TP3-ALEJO_ALMADA.py
+++ Programacion 1/3- Estructuras Condicionales/TP3-ALEJO_ALMADA.py
@@ -5,3 +5,43 @@
 # edad = int(input("Ingrese su edad: "))
 # if edad >= MAYOR_EDAD:
 #     print("Es mayor de edad.") 
+
+# 2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+# mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+# mensaje “Desaprobado”.
+
+# NOTA_APROBADO = 6
+# nota_alumno = int(input("Ingrese su nota: "))
+# if nota_alumno >= NOTA_APROBADO:
+#     print("Aprobado, ¡felicitaciones!")
+# else: 
+#     print("Desaprobado, debes esforzarte más.")
+
+# 3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+# número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+# contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+# operador de módulo (%) en Python para evaluar si un número es par o impar.
+
+# numero = int(input("Ingrese un numero par: "))
+# if numero % 2 == 0: 
+#     print("Ha ingresado un número par.")
+# else: 
+#     print("Por favor, ingrese un número par.")
+    
+
+# 4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+# siguientes categorías pertenece:
+# ● Niño/a: menor de 12 años.
+# ● Adolescente: mayor o igual que 12 años y menor que 18 años.
+# ● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+# ● Adulto/a: mayor o igual que 30 años.
+
+# edad_usuario = int(input("Ingrese su edad: "))
+# if edad_usuario > 0 and edad_usuario < 12:
+#     print("Eres un niño/a.")
+# elif edad_usuario >= 12 and edad_usuario < 18:
+#     print("Eres adolescente.")
+# elif edad_usuario >= 18 and edad_usuario < 30:
+#     print("Eres adulto/a joven.")
+# elif edad_usuario >= 30:
+#     print("Eres adulto/a.")
