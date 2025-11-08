cac5e2130a5b2c87a493493f6f154cbb6fadcf32
Alejo Almada <alejonahuelalmada@gmail.com>
2025-11-07 21:22:09 -0300

    Se realizo tp 8

--- 7- Datos Complejos/TP7- ALEJO ALMADA.py
+++ 7- Datos Complejos/TP7- ALEJO ALMADA.py
@@ -1,3 +1,5 @@
+# ACLARACION : SE REALIZARON ALGUNOS ELJERCICIOS PARA REFORZAR LA UNIDAD NO LA TOTALIDAD.
+
 # 1) Dado el diccionario precios_frutas
 # precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
 # 1450}
@@ -6,14 +8,14 @@
 # ● Manzana = 1500
 # ● Pera = 2300
 
-# precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
-#  1450}
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+ 1450}
 
-# precios_frutas["Naranja"] = 1200
-# precios_frutas["Manzana"] = 1500
-# precios_frutas["Pera"] = 2300
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
 
-# print(precios_frutas)
+print(precios_frutas)
 
 # 2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
 # desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
@@ -21,73 +23,73 @@
 # ● Manzana = 1700
 # ● Melón = 2800
 
-# precios_frutas["Banana"] = 1330
-# precios_frutas["Manzana"] = 1700
-# precios_frutas["Melón"] = 2800
+precios_frutas["Banana"] = 1330
+precios_frutas["Manzana"] = 1700
+precios_frutas["Melón"] = 2800
 
-# print(precios_frutas)
+print(precios_frutas)
 
 # 3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
 # desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los
 # precios
 
-# lista_frutas = []
-# for i in precios_frutas:
-#     lista_frutas.append(i) 
+lista_frutas = []
+for i in precios_frutas:
+    lista_frutas.append(i) 
     
-# print(lista_frutas)
+print(lista_frutas)
 
 # 4) Escribí un programa que permita almacenar y consultar números telefónicos.
 # • Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.
 # • Luego, pedí un nombre y mostrale el número asociado, si existe.
 
-# contactos = {}
-# for i in range(5):
-#     nuevo_contacto = input("Ingrese un nuevo contacto: ")
-#     nuevo_numero = input("Ingrese el numero telefonico: ")
-#     contactos[nuevo_contacto] = nuevo_numero
+contactos = {}
+for i in range(5):
+    nuevo_contacto = input("Ingrese un nuevo contacto: ")
+    nuevo_numero = input("Ingrese el numero telefonico: ")
+    contactos[nuevo_contacto] = nuevo_numero
     
 
-# while True:
-#     buscar_nombre = input("Ingrese el nombre del contacto que busca: ")
-#     if buscar_nombre in contactos.keys():
-#         print(f"El numero es {contactos[buscar_nombre]}")
-#     else:
-#         print("El contacto no existe. Intente nuevavente")
+while True:
+    buscar_nombre = input("Ingrese el nombre del contacto que busca: ")
+    if buscar_nombre in contactos.keys():
+        print(f"El numero es {contactos[buscar_nombre]}")
+    else:
+        print("El contacto no existe. Intente nuevavente")
 
 # 5) Solicita al usuario una frase e imprime:
 # • Las palabras únicas (usando un set).
 # • Un diccionario con la cantidad de veces que aparece cada palabra
 
-# frase = input("Ingrese una frase separada por espacios: ")
+frase = input("Ingrese una frase separada por espacios: ")
 
-# #Colocamos todas las palabras en minusculas para evitar errores
-# frase = frase.lower()
+#Colocamos todas las palabras en minusculas para evitar errores
+frase = frase.lower()
 
-# #Separamos la frase en plabras
-# palabras = frase.split()
+#Separamos la frase en plabras
+palabras = frase.split()
 
-# #Guardamos las palabras unicas
-# palabras_unicas = set(palabras)
+#Guardamos las palabras unicas
+palabras_unicas = set(palabras)
 
-# #Creamos el diccionario
-# cantidad_palabras = {}
+#Creamos el diccionario
+cantidad_palabras = {}
 
-# #Con un ciclo for contamos la cantidad de veces que se repte cada palabra
+#Con un ciclo for contamos la cantidad de veces que se repte cada palabra
 
-# for palabra in palabras:
-#     if palabra in cantidad_palabras:
-#         cantidad_palabras[palabra] += 1
-#     else:
-#         cantidad_palabras[palabra] = 1
+for palabra in palabras:
+    if palabra in cantidad_palabras:
+        cantidad_palabras[palabra] += 1
+    else:
+        cantidad_palabras[palabra] = 1
 
-# #Mostramos por pantalla
+#Mostramos por pantalla
 
-# print("Palabras unicas: ")
-# print(palabras_unicas)
+print("Palabras unicas: ")
+print(palabras_unicas)
 
-# print("Cantidad de veces qie aparece cada palabra: ")
-# print(cantidad_palabras)
+print("Cantidad de veces qie aparece cada palabra: ")
+print(cantidad_palabras)
 
 # 8) Armá un diccionario donde las claves sean nombres de productos y los valores su stock.
 # Permití al usuario:
--- 8- Manejo de archivos/TP8- ALEJO ALMADA.py
+++ 8- Manejo de archivos/TP8- ALEJO ALMADA.py
@@ -1,79 +1,63 @@
-# print("1. Escribiendo archivo")
-# archivo_productos = open("productos.txt", "w")
-# archivo_productos.write("\nLapicera")
-# archivo_productos.write("\nPincel")
-# archivo_productos.close
+# # print("1. Escribiendo archivo")
+# # archivo_productos = open("productos.txt", "w")
+# # archivo_productos.write("\nLapicera")
+# # archivo_productos.write("\nPincel")
+# # archivo_productos.close
 
-# print("2. Leyendo archivo")
-# archivo_productos = open("productos.txt", "r")
-# contenido = archivo_productos.read()
-# print(contenido)
-# archivo_productos.close
+# # print("2. Leyendo archivo")
+# # archivo_productos = open("productos.txt", "r")
+# # contenido = archivo_productos.read()
+# # print(contenido)
+# # archivo_productos.close
 
-# print("3. Agregando texto al final del archivo.")
-# archivo_productos = open("productos.txt", "a")
-# archivo_productos.write("Goma")
-# archivo_productos.close
+# # print("3. Agregando texto al final del archivo.")
+# # archivo_productos = open("productos.txt", "a")
+# # archivo_productos.write("Goma")
+# # archivo_productos.close
 
-# print("4. Sobrescribiendo contenido")
-# archivo_productos = open("productos.txt", "w")
-# archivo_productos.write("Regla\n")
-# archivo_productos.close
+# # print("4. Sobrescribiendo contenido")
+# # archivo_productos = open("productos.txt", "w")
+# # archivo_productos.write("Regla\n")
+# # archivo_productos.close
 
 
-# print("2. Leyendo archivo")
-# archivo_productos = open("productos.txt", "r")
-# contenido = archivo_productos.read()
-# print(contenido)
-# archivo_productos.close
+# # print("2. Leyendo archivo")
+# # archivo_productos = open("productos.txt", "r")
+# # contenido = archivo_productos.read()
+# # print(contenido)
+# # archivo_productos.close
 
-# print("9. Leyendo archivo con with")
-# with  open("productos.txt", "r") as archivo:
-#     contenido = archivo.read()
-#     print(contenido)
+# # print("9. Leyendo archivo con with")
+# # with  open("productos.txt", "r") as archivo:
+# #     contenido = archivo.read()
+# #     print(contenido)
 
-# print("10. Creando contenido con with")
-# with open("productos.txt", "a") as archivo:
-#     archivo.write("\nCartuchera")
+# # print("10. Creando contenido con with")
+# # with open("productos.txt", "a") as archivo:
+# #     archivo.write("\nCartuchera")
 
-# print("11. Leyendo texto")
-# with open("productos.txt", "r") as archivo:
-#     contenido = archivo.read()
-#     print(contenido)
+# # print("11. Leyendo texto")
+# # with open("productos.txt", "r") as archivo:
+# #     contenido = archivo.read()
+# #     print(contenido)
 
-# 1. Crear archivo inicial con productos: Crear un archivo de texto llamado
-# productos.txt con tres productos. Cada línea debe tener: nombre,precio,cantidad
+# # 1. Crear archivo inicial con productos: Crear un archivo de texto llamado
+# # productos.txt con tres productos. Cada línea debe tener: nombre,precio,cantidad
 
-archivo_inicial = open("productos.txt", "w")
-archivo_inicial.write("nombre\n")
-archivo_inicial.write("precio\n")
-archivo_inicial.write("cantidad\n")
-archivo_inicial.close
+# archivo_inicial = open("productos.txt", "w")
+# archivo_inicial.write("nombre\n")
+# archivo_inicial.write("precio\n")
+# archivo_inicial.write("cantidad\n")
+# archivo_inicial.close
 
-# 2. Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada
-# línea, la procese con .strip() y .split(","), y muestre los productos en el siguiente
-# formato:
-# Producto: Lapicera | Precio: $120.5 | Cantidad: 30
-with open("productos.txt", "w") as archivo:
-    archivo.write("Lapicera,120.5,30\n")
-    archivo.write("Cuaderno,300.0,15\n")
-    archivo.write("Goma,80.0,50\n")
-
-with open("productos.txt", "r") as archivo:
-    for linea in archivo:
-        linea = linea.strip()
-        partes = linea.split(",")
-        nombre, precio, cantidad = partes
-        print(f"Producto: {nombre} | Precio: ${precio} | Cantidad: {cantidad}")
-
-# 3. Agregar productos desde teclado: Modificar el programa para que luego de mostrar
-# los productos, le pida al usuario que ingrese un nuevo producto (nombre, precio,
-# cantidad) y lo agregue al archivo sin borrar el contenido existente.
-
-nuevo_producto = input("Ingrese el nuevo producto con el siguiente formato: 'producto, precio, cantidad': ")
-
-with open("productos.txt", "a") as archivo:
-    archivo.write(nuevo_producto)
+# # 2. Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada
+# # línea, la procese con .strip() y .split(","), y muestre los productos en el siguiente
+# # formato:
+# # Producto: Lapicera | Precio: $120.5 | Cantidad: 30
+# with open("productos.txt", "w") as archivo:
+#     archivo.write("Lapicera,120.5,30\n")
+#     archivo.write("Cuaderno,300.0,15\n")
+#     archivo.write("Goma,80.0,50\n")
 
 # with open("productos.txt", "r") as archivo:
 #     for linea in archivo:
@@ -81,3 +65,48 @@ with open("productos.txt", "a") as archivo:
 #         partes = linea.split(",")
 #         nombre, precio, cantidad = partes
 #         print(f"Producto: {nombre} | Precio: ${precio} | Cantidad: {cantidad}")
+
+# # 3. Agregar productos desde teclado: Modificar el programa para que luego de mostrar
+# # los productos, le pida al usuario que ingrese un nuevo producto (nombre, precio,
+# # cantidad) y lo agregue al archivo sin borrar el contenido existente.
+
+# nuevo_producto = input("Ingrese el nuevo producto con el siguiente formato: 'producto, precio, cantidad': ")
+
+# with open("productos.txt", "a") as archivo:
+#     archivo.write(nuevo_producto)
+
+# with open("productos.txt", "r") as archivo:
+#     for linea in archivo:
+#         linea = linea.strip()
+#         partes = linea.split(",")
+#         nombre, precio, cantidad = partes
+#         print(f"Producto: {nombre} | Precio: ${precio} | Cantidad: {cantidad}")
+
+# 4. Cargar productos en una lista de diccionarios: Al leer el archivo, cargar los datos en
+# una lista llamada productos, donde cada elemento sea un diccionario con claves:
+# nombre, precio, cantidad.
+productos_lista = [
+    "lapicera,12.5,20\n",
+    "pincel,15.0,12\n", 
+    "goma,2.5,50\n"
+
+    ]
+
+
+with open("productos.txt", "w") as archivo:
+    archivo.writelines(productos_lista)
+
+
+prducto ={}
+productos = []
+with open("productos.txt", "r") as archivo:
+    for linea in archivo:
+        linea = linea.strip()
+        partes = linea.split(",")
+        producto = {"nombre" : partes[0], "precio" : partes[1], "cantidad" : partes[2]  }
+        productos.append(producto)
+print(productos)
+
+    
+
+    
